d508fbf2464030bb0fbb098e1c1d7702ca609742
Jayce <kendump666@gmail.com>
2024-07-10 15:54:37 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -43,7 +43,7 @@ onMounted(() => {
       url0 = params.get("url");
       tvlistUrl = params.get("s");
     }
-    tvlistUrl = tvlistUrl || localStorage.getItem("tvlistUrl") || "https://raw.githubusercontent.com/cee-tv/Chinatv/main/list.txt";
+    tvlistUrl = tvlistUrl || localStorage.getItem("tvlistUrl") || "https://raw.githubusercontent.com/cee-tv/weekendiptv/main/Channel.txt";
     let suffixName = tvlistUrl && suffix(tvlistUrl);
     if (tvlistUrl) {
       if (suffixName == "m3u8") {
